dd4d9b2b11ab5b361c60533bff91e2e8487046e5
Timothy Langham <graniteblood@gmail.com>
2021-03-12 17:42:46 -0500

    mini child emergency getting back to work

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,28 @@
 
 
 const Header = (title, date, temp) => {
-  constructor(headerAppender) 
-    this.title = title;
-    this.date = date;
-    this.temp = temp;
+
+
+  const header = document.createElement('div');
+  header.classList.add('header')
+
+  const date = document.createElement('span');
+  date.classList.add('date')  
+  date.textContent = ` ${ date }`
+
+  const title = document.createElement('h1');
+  h1.textContent = ` ${ title }`
+
+  const temp = document.createElement('span');
+  temp.classList.add('temp')
+  temp.textContent = ` ${temp}`
+
+
+  // constructor(headerAppender) 
+  //   this.title = title;
+  //   this.date = date;
+  //   this.temp = temp;
   
-
-  // const header = document.createElement('div');
-  // header.classList.add('header')
-
-  // const date = document.createElement('span');
-  // date.classList.add('date')  
-  // date.textContent = ` ${ date }`
-
-  // const title = document.createElement('h1');
-  // h1.textContent = ` ${ title }`
-
-  // const temp = document.createElement('span');
-  // temp.classList.add('temp')
-  // temp.textContent = ` ${temp}`
 
 
   // TASK 1
@@ -37,6 +40,10 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+
+
+
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
